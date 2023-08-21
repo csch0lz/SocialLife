fa145f8498c72f7b66375ed831b2c4adf541a450
csch0lz <cscholz@asc.upenn.edu>
2023-08-21 12:35:07 +0200

    img

--- scripts/makeEmoCorMat.R
+++ scripts/makeEmoCorMat.R
@@ -33,10 +33,10 @@ emo_anti_prof=emo_df_all %>%mutate(val_cond=case_when(grepl('non',condition)~'no
   left_join(drink_all_agg) %>% 
   filter(val_cond=='anti-alcohol',source_cond=='professional') %>%
   dplyr::select(all_of(emo_vars),drink_rating) %>% 
-  rename(embarrassed="embarrassed (for someone else)",`drinking urge`='drink_rating')
+  rename(embarrassed="embarrassed (for someone else)",`alcohol craving`='drink_rating')
 
 emo_pvals_anti_prof = emo_anti_prof %>% pivot_longer(cols=c(any_of(emo_vars),'embarrassed'),names_to='emotion') %>%
-  group_by(emotion) %>% summarise(p=cor.test(value,`drinking urge`)$p.value) %>%
+  group_by(emotion) %>% summarise(p=cor.test(value,`alcohol craving`)$p.value) %>%
   mutate(emotion=factor(emotion,levels=c('positive','happy','jealous','amused','negative','embarrassed','sad','shocked','scared'))) %>% 
   arrange(emotion) %>%
   mutate(psig=case_when(p<0.001~'p<.00025',TRUE~'not significant'))
@@ -45,12 +45,12 @@ emo_cormat_anti_prof=emo_anti_prof %>%
   cor %>%
   get_lower_tri %>%
   melt(.,na.rm=T) %>% 
-  filter(Var1=="drinking urge" | Var2=="drinking urge") %>%
-  mutate(Var1=factor(Var1,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','drinking urge')),
-         Var2=factor(Var2,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','drinking urge'))
+  filter(Var1=="alcohol craving" | Var2=="alcohol craving") %>%
+  mutate(Var1=factor(Var1,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','alcohol craving')),
+         Var2=factor(Var2,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','alcohol craving'))
   ) %>%
   left_join(emo_pvals_anti_prof,by=c('Var2'='emotion')) %>%
-  mutate(psig=ifelse(Var2=='drinking urge','p<.00025',psig)) 
+  mutate(psig=ifelse(Var2=='alcohol craving','p<.00025',psig)) 
 
 emo_plot_anti_prof=emo_cormat_anti_prof %>% 
   mutate(Var2=factor(Var2,levels = Var2[order(value)])) %>%
@@ -60,7 +60,7 @@ emo_plot_anti_prof=emo_cormat_anti_prof %>%
   scale_pattern_discrete(choices=c('circle','none')) + 
   coord_fixed() +
   labs(x='',y='') +
-  ggtitle('Anti-Alcohol (Prof.)')+
+  ggtitle('Anti - Prof.')+
   theme(axis.text.x=element_text(angle=45,vjust=1, size=8, hjust=1),legend.position='none')
 
 ###ANTI-ALCOHOL (Peer-Produced)
@@ -78,10 +78,10 @@ emo_anti_peer=emo_df_all %>%mutate(val_cond=case_when(grepl('non',condition)~'no
   left_join(drink_all_agg) %>% 
   filter(val_cond=='anti-alcohol',source_cond=='peer-produced') %>%
   dplyr::select(all_of(emo_vars),drink_rating) %>% 
-  rename(embarrassed="embarrassed (for someone else)",`drinking urge`='drink_rating')
+  rename(embarrassed="embarrassed (for someone else)",`alcohol craving`='drink_rating')
 
 emo_pvals_anti_peer = emo_anti_peer %>% pivot_longer(cols=c(any_of(emo_vars),'embarrassed'),names_to='emotion') %>%
-  group_by(emotion) %>% summarise(p=cor.test(value,`drinking urge`)$p.value) %>%
+  group_by(emotion) %>% summarise(p=cor.test(value,`alcohol craving`)$p.value) %>%
   mutate(emotion=factor(emotion,levels=c('positive','happy','jealous','amused','negative','embarrassed','sad','shocked','scared'))) %>% 
   arrange(emotion) %>%
   mutate(psig=case_when(p<0.001~'p<.00025',TRUE~'not significant'))
@@ -90,12 +90,12 @@ emo_cormat_anti_peer=emo_anti_peer %>%
   cor %>%
   get_lower_tri %>%
   melt(.,na.rm=T) %>% 
-  filter(Var1=="drinking urge" | Var2=="drinking urge") %>%
-  mutate(Var1=factor(Var1,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','drinking urge')),
-         Var2=factor(Var2,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','drinking urge'))
+  filter(Var1=="alcohol craving" | Var2=="alcohol craving") %>%
+  mutate(Var1=factor(Var1,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','alcohol craving')),
+         Var2=factor(Var2,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','alcohol craving'))
   ) %>%
   left_join(emo_pvals_anti_peer,by=c('Var2'='emotion')) %>%
-  mutate(psig=ifelse(Var2=='drinking urge','p<.00025',psig)) 
+  mutate(psig=ifelse(Var2=='alcohol craving','p<.00025',psig)) 
 
 emo_plot_anti_peer=emo_cormat_anti_peer %>% 
   mutate(Var2=factor(Var2,levels = Var2[order(value)])) %>%
@@ -105,7 +105,7 @@ emo_plot_anti_peer=emo_cormat_anti_peer %>%
   scale_pattern_discrete(choices=c('circle','none')) + 
   coord_fixed() +
   labs(x='',y='') +
-  ggtitle('Anti-Alcohol (Peer-Prod.)')+
+  ggtitle('Anti - Peer')+
   theme(axis.text.x=element_text(angle=45,vjust=1, size=8, hjust=1),legend.position='none')
 
 ###PRO-ALCOHOL
@@ -120,10 +120,10 @@ emo_pro=emo_df_all %>%mutate(val_cond=case_when(grepl('non',condition)~'non-alco
   left_join(drink_all_agg) %>% 
   filter(val_cond=='pro-alcohol') %>%
   dplyr::select(all_of(emo_vars),drink_rating) %>% 
-  rename(embarrassed="embarrassed (for someone else)",`drinking urge`='drink_rating')
+  rename(embarrassed="embarrassed (for someone else)",`alcohol craving`='drink_rating')
 
 emo_pvals_pro = emo_pro %>% pivot_longer(cols=c(any_of(emo_vars),'embarrassed'),names_to='emotion') %>%
-  group_by(emotion) %>% summarise(p=cor.test(value,`drinking urge`)$p.value) %>%
+  group_by(emotion) %>% summarise(p=cor.test(value,`alcohol craving`)$p.value) %>%
   mutate(emotion=factor(emotion,levels=c('positive','happy','jealous','amused','negative','embarrassed','sad','shocked','scared'))) %>%
   arrange(emotion) %>%
   mutate(psig=case_when(p<0.00025~'p<.00025',TRUE~'not significant'))
@@ -132,12 +132,12 @@ emo_cormat_pro=emo_pro %>%
   cor %>%
   get_lower_tri %>%
   melt(.,na.rm=T) %>% 
-  filter(Var1=="drinking urge" | Var2=="drinking urge") %>%
-  mutate(Var1=factor(Var1,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','drinking urge')),
-         Var2=factor(Var2,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','drinking urge'))
+  filter(Var1=="alcohol craving" | Var2=="alcohol craving") %>%
+  mutate(Var1=factor(Var1,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','alcohol craving')),
+         Var2=factor(Var2,levels=c('scared','shocked','sad','embarrassed','negative','amused','jealous','happy','positive','alcohol craving'))
   ) %>%
   left_join(emo_pvals_pro,by=c('Var2'='emotion')) %>%
-  mutate(psig=ifelse(Var2=='drinking urge','p<.00025',psig)) 
+  mutate(psig=ifelse(Var2=='alcohol craving','p<.00025',psig)) 
 
 emo_plot_pro=emo_cormat_pro %>% 
   mutate(Var2=factor(Var2,levels = Var2[order(value)])) %>%
@@ -147,9 +147,9 @@ emo_plot_pro=emo_cormat_pro %>%
   scale_pattern_discrete(choices=c('circle','none'),guide=guide_legend(title='Significance Pattern',override.aes=list(fill='white',color='black'))) +  
   coord_fixed() +
   labs(x='',y='') +
-  ggtitle('Pro-Alcohol')+
+  ggtitle('Pro')+
   theme(axis.text.x=element_text(angle=45,vjust=1, size=8, hjust=1),legend.position='right')
 
 final=ggarrange(emo_plot_anti_prof,emo_plot_anti_peer,emo_plot_pro,nrow=1)
 
-ggsave('Figures/EmoCorMat_Figure.png', plot = final)
+ggsave('Figures/EmoCorMat_Figure.png', plot = final,width=1200,height=589,units='px')
